91de699dfd0b8878fb3f43df91d8c92479d2b614
Daniel Durán Cassani <danielduran1995@gmail.com>
2024-09-19 10:22:29 +0200

    Add files via upload

---
+++ src/test/java/Indra7/ejercicio7/Ejercicio7ApplicationTests.java
@@ -0,0 +1,13 @@
+package Indra7.ejercicio7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
